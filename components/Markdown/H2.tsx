43309c8503107b8ae285998e6b883da538a67001
Justin Duke <justin@buttondown.email>
2022-07-15 10:59:38 -0400

    Increase spacing between paragraphs

--- components/Markdown/H2.tsx
+++ components/Markdown/H2.tsx
@@ -1,5 +1,7 @@
 const H2 = ({ children }) => {
-  return <h2 className="text-3xl mb-4 font-bold font-serif">{children}</h2>;
+  return (
+    <h2 className="text-3xl mb-4 mt-20 font-bold font-serif">{children}</h2>
+  );
 };
 
 export default H2;
--- components/Markdown/H3.tsx
+++ components/Markdown/H3.tsx
@@ -1,5 +1,7 @@
 const H3 = ({ children }) => {
-  return <h3 className="text-2xl mb-4 font-bold font-serif">{children}</h3>;
+  return (
+    <h3 className="text-2xl mb-4 mt-16 font-bold font-serif">{children}</h3>
+  );
 };
 
 export default H3;
