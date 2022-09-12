179b143f12511de2722516c097c12137e469319e
Justin Duke <justin@buttondown.email>
2022-09-12 11:26:58 -0400

    Fix bug where new-tab wasn't working in content catalogs

--- components/Catalog/ContentCatalog.tsx
+++ components/Catalog/ContentCatalog.tsx
@@ -33,29 +33,33 @@ const ContentCatalog = ({
       items={items}
       lefthandComponent={(item) => (
         <Link href={`/catalogs/${name}/${item.slug}`}>
-          <div className="cursor-pointer relative group">
-            <a className="group-hover:brightness-0">
-              <ImageColophon image={item.image} alt={item.title} />
-            </a>
-            <div className="absolute top-4 bottom-4 left-8 right-8 invisible group-hover:visible">
-              <div className="items-center text-center flex h-48">
-                <div className="m-auto text-white">
-                  <H3>{item.title}</H3>
-                  <div className="text-brand">
-                    {"✭".repeat(Math.round(item.rating / 2))}
+          <a href={`/catalogs/${name}/${item.slug}`}>
+            <div className="cursor-pointer relative group">
+              <a className="group-hover:brightness-0">
+                <ImageColophon image={item.image} alt={item.title} />
+              </a>
+              <div className="absolute top-4 bottom-4 left-8 right-8 invisible group-hover:visible">
+                <div className="items-center text-center flex h-48">
+                  <div className="m-auto text-white">
+                    <H3>{item.title}</H3>
+                    <div className="text-brand">
+                      {"✭".repeat(Math.round(item.rating / 2))}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </a>
         </Link>
       )}
       righthandComponent={(item) => (
         <div className="flex-1">
           <Link href={`/catalogs/${name}/${item.slug}`}>
-            <div className="cursor-pointer hover:text-brand -mt-20">
-              <H2>{item.title}</H2>
-            </div>
+            <a href={`/catalogs/${name}/${item.slug}`}>
+              <div className="cursor-pointer hover:text-brand -mt-20">
+                <H2>{item.title}</H2>
+              </div>
+            </a>
           </Link>
           {item.author && item.year && (
             <div className="text-lg uppercase text-gray-600 -mt-4">
